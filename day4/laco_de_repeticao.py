f10f9f9e69edb3f8c1a7a4dfdd1e34c90a80e479
Júlio César <jcesar1579@gmail.com>
2024-11-21 11:29:46 -0300

    Adicioanando dia 4 laço de repetição

---
+++ day4/laco_de_repeticao.py
@@ -0,0 +1,13 @@
+#usando for para criar uma laço de repetição
+
+#for numero in range (11):
+#    print(numero)
+
+
+#Usando While para criar o laço de repetição
+
+numero = 0
+
+while numero < 11:
+    print(numero)
+    numero +=1
