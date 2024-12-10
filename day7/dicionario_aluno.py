c209dc24bf6e020e36d1bcfdd79bcb3c1fd65d18
Júlio César <jcesar1579@gmail.com>
2024-12-10 14:53:49 -0300

    add o dia 7 dicionarios dinamico e estatico

---
+++ day7/dicionario_aluno.py
@@ -0,0 +1,11 @@
+aluno = {
+    "nome": "Julio",
+    "idade": "26",
+    "Curso": "Python do Zero ao hacking",
+    "Matricula": 7584817212
+}
+
+print("Nome:", aluno["nome"])
+print("Idade:", aluno['idade'])
+print("Curso:", aluno['Curso'])
+print("Matricula:", aluno["Matricula"])
