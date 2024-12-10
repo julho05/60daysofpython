178beac2919b36438296953315bcef8523dcd59f
Júlio César <jcesar1579@gmail.com>
2024-12-10 11:53:39 -0300

    add dia 6 lista de frutas

---
+++ day6/listafrutas.py
@@ -0,0 +1,18 @@
+#lista de frutas
+
+#frutas = ["banana", "maça", "uva","pera", "manga"]
+
+#for fruta in frutas:
+#    print(fruta)
+
+#Utilizando o input para criar a lista de frutas 
+
+frutas = []
+
+while True:
+    fruta = input("Digite o nome da fruta: ")
+    if fruta == 'sair':
+        break
+    frutas.append(fruta)
+    
+print(frutas)
