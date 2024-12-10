e7ef1ea775ad95f1c53ed546c323da296669bff6
Júlio César <jcesar1579@gmail.com>
2024-12-10 11:39:28 -0300

    add o dia 5 função quadrado

---
+++ day5/funcaoquadrado.py
@@ -0,0 +1,12 @@
+#Função simples em Python que retorna o seu quadrado
+
+
+def quadrado_numero(numero):
+    return numero ** 2
+
+def quadrado_numerov2(numero):
+    return numero * numero
+
+print(quadrado_numero(5))
+print(quadrado_numerov2(5))
+
