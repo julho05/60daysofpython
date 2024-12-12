969b8ea4c457344504bc6a37b66b087fe3de8f32
Júlio César <jcesar1579@gmail.com>
2024-12-12 10:41:21 -0300

    add dia 8 check idade

---
+++ day8/condicional_idade.py
@@ -0,0 +1,10 @@
+#solicitar a idade do usuario e ver se ele pode dirigir
+
+idade = int(input('Digite sua idade: '))
+
+if idade >= 18:
+    print('Você pode dirigir')
+else:
+    print('Você não pode dirigir.')
+
+
