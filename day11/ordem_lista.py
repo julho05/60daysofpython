db2207db2de8cc61f4c11a377e769d24aaa866b5
Júlio César <jcesar1579@gmail.com>
2024-12-17 09:57:49 -0300

    add dia 11 criando uma lista ordenada apartir  do sorted()

---
+++ day11/ordem_lista.py
@@ -0,0 +1,14 @@
+#lista ordenada a partir do metodo sorted()
+
+#1. lista numerica
+
+list_num = [55,69,31,66,77,121,661,1,2,3,4]
+
+#sorted() eh uma funcao embutida no python
+#aceita uma lista e deixar ela em ordem
+
+numeros_ordenados = sorted(list_num)
+numeros_ordenados2 = sorted(list_num, reverse=True)
+
+print(numeros_ordenados)
+print(numeros_ordenados2)
